dda1c3ad71d93aa222e8c7e0844c3b3e3b6e8897
Peiiyong <165624659+Peiiyong@users.noreply.github.com>
2024-12-28 14:44:18 +0800

    book

--- Book/Book.java
+++ Book/Book.java
@@ -1,3 +1,40 @@
+package Book;
+
 public class Book {
-    
+    String title;
+    String author;
+    String isbn;
+    boolean isAvailable;
+
+    public Book(String title, String author, String isbn, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public String printBookInfo() {
+        return "Title: "+title+", Author: "+author+", ISBN: "+isbn+", Available: "+isAvailable;
+    }
 }
+
